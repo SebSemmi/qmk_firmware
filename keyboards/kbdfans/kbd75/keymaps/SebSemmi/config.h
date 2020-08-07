4cf6a7d0d0004ecf805d796ba6b2fb667e3abb0f
Sebastian Semmler <sebastian@semmler.pro>
2020-08-08 01:09:34 +0200

    initialition KBD75v2 layout

---
+++ keyboards/kbdfans/kbd75/keymaps/SebSemmi/config.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#undef VENDOR_ID
+#undef PRODUCT_ID
+#undef MANUFACTURER
+#undef PRODUCT
+#undef DESCRIPTION
+
+#define VENDOR_ID 0x05ac
+#define PRODUCT_ID 0x024f
+#define MANUFACTURER SebSemmi
+#define PRODUCT KBD75v2
+#define DESCRIPTION a KBD75v2 with macOS ISO-DE optimations
+
+/* turn off RGB when computer sleeps */
+#ifdef RGB_DI_PIN
+#define RGBLIGHT_SLEEP
+#endif
