a70119d59dfd30c763c94872d3e1668cda3278e7
Sebastian Semmler <sebastian@semmler.pro>
2020-08-08 20:15:57 +0200

    changed Product ID to Apple Keyboard Wired ISO-DE

--- keyboards/kbdfans/kbd75/keymaps/SebSemmi/config.h
+++ keyboards/kbdfans/kbd75/keymaps/SebSemmi/config.h
@@ -7,10 +7,10 @@
 #undef DESCRIPTION
 
 #define VENDOR_ID 0x05ac
-#define PRODUCT_ID 0x024f
+#define PRODUCT_ID 0x0221
 #define MANUFACTURER SebSemmi
 #define PRODUCT KBD75v2
-#define DESCRIPTION a KBD75v2 with macOS ISO-DE optimations
+#define DESCRIPTION a KBD75v2 with macOS ISO-DE Layout
 
 /* turn off RGB when computer sleeps */
 #ifdef RGB_DI_PIN
