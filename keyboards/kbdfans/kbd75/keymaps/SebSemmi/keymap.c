a990a9a832e8f99fcfab4f1f213c9bb286e7b93a
Sebastian Semmler <sebastian@semmler.pro>
2020-09-12 23:20:35 +0200

    changed RGB Lighting control scheme

--- keyboards/kbdfans/kbd75/keymaps/SebSemmi/keymap.c
+++ keyboards/kbdfans/kbd75/keymaps/SebSemmi/keymap.c
@@ -6,41 +6,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC, KC_DEL, 
         KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_PGUP, 
         KC_CAPS, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_NUHS, KC_ENT, KC_PGDN, 
-        KC_LSFT, KC_NUBS, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_UP, TG(1), 
+        KC_LSFT, KC_NUBS, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_UP, MO(1), 
         KC_LCTL, KC_LALT, KC_LGUI, KC_SPC, KC_RGUI, KC_RALT, KC_APFN, KC_LEFT, KC_DOWN, KC_RGHT),
 	[1] = LAYOUT_75_iso(
-        KC_ESC, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_WAKE, KC_SLEP, KC_PWR, 
-        KC_NO, KC_PSLS, KC_PAST, KC_PMNS, KC_PPLS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC__MUTE, 
-        KC_NO, KC_P7, KC_P8, KC_P9, KC_PEQL, KC_NO, RGB_TOG, RGB_MOD, RGB_RMOD, KC_NO, KC_NO, KC_NO, NK_ON, KC__VOLUP, 
-        KC_NO, KC_P4, KC_P5, KC_P6, KC_NO, KC_NO, RGB_HUI, RGB_HUD, RGB_VAI, RGB_VAD, KC_NO, KC_NO, NK_OFF, KC_NO, KC__VOLDOWN, 
-        KC_NO, KC_P0, KC_P1, KC_P2, KC_P3, KC_PDOT, KC_NO, RGB_SAI, RGB_SAD, RGB_SPI, RGB_SPD, KC_NO, NK_TOGG, KC_NO, KC_TRNS, 
-        KC_NO, KC_NO, KC_PENT, KC_NO, KC_NO, KC_NO, KC_TRNS, KC_MPRV, KC_MPLY, KC_MNXT)
+        RGB_TOG, RGB_MOD, RGB_RMOD, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_WAKE, KC_SLEP, KC_PWR, 
+        KC_NO, RGB_HUI, RGB_SAI, RGB_VAI, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, 
+        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, 
+        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, 
+        KC_LSFT, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_RSFT, KC_NO, KC_TRNS, 
+        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO)
 };
-// layer colors
-layer_state_t layer_state_set_user(layer_state_t state) {
-    switch (get_highest_layer(state)) {
-    case 1:
-        rgblight_enable_noeeprom();
-        rgblight_sethsv_noeeprom_orange();
-        rgblight_mode_noeeprom(1);
-        break;
-    default: //  for any other layers, or the default layer
-        rgblight_disable_noeeprom();
-        break;
-    }
-  return state;
-}
-
-// layer colors
-// layer_state_t layer_state_set_user(layer_state_t state) {
-//     switch (get_highest_layer(state)) {
-//     case 1:
-//         rgblight_sethsv_noeeprom_chartreuse();
-//         rgblight_mode_noeeprom(1);
-//         break;
-//     default: //  for any other layers, or the default layer
-//         rgblight_mode_noeeprom(9); //rainbow swirl
-//         break;
-//     }
-//   return state;
-// }
