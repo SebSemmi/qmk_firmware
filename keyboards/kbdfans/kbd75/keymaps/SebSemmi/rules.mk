83dcde14b11d1f965467ed9f24aa5984111f021c
Sebastian Semmler <sebastian@semmler.pro>
2020-09-13 14:40:49 +0200

    cosmetics

--- keyboards/kbdfans/kbd75/keymaps/SebSemmi/rules.mk
+++ keyboards/kbdfans/kbd75/keymaps/SebSemmi/rules.mk
@@ -13,17 +13,16 @@ BOOTLOADER = atmel-dfu
 
 # Build Options
 #   comment out to disable the options.
-#
-BOOTMAGIC_ENABLE = full	# Virtual DIP switch configuration
-MOUSEKEY_ENABLE = yes	# Mouse keys
-EXTRAKEY_ENABLE = yes	# Audio control and System control
-CONSOLE_ENABLE = no	# Console for debug
-COMMAND_ENABLE = no    # Commands for debug and configuration
-SLEEP_LED_ENABLE = no  # Breathing sleep LED during USB suspend
-NKRO_ENABLE = no		# USB Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
-BACKLIGHT_ENABLE = no  # Enable keyboard backlight functionality
-AUDIO_ENABLE = no
-RGBLIGHT_ENABLE = yes
-APPLE_FN_ENABLE = yes
+BOOTMAGIC_ENABLE = full		# Virtual DIP switch configuration
+MOUSEKEY_ENABLE = yes		# Mouse keys
+EXTRAKEY_ENABLE = yes		# Audio control and System control
+CONSOLE_ENABLE = no			# Console for debug
+COMMAND_ENABLE = no    		# Commands for debug and configuration
+SLEEP_LED_ENABLE = no  		# Breathing sleep LED during USB suspend
+NKRO_ENABLE = no			# USB Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
+BACKLIGHT_ENABLE = no  		# Enable keyboard backlight functionality
+AUDIO_ENABLE = no			# Audio output
+RGBLIGHT_ENABLE = yes		# Enable keyboard RGB underglow
+APPLE_FN_ENABLE = yes		# Enable Apple FN-Key
 
 LAYOUTS = 75_iso
