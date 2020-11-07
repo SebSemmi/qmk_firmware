4574b1af2fcefac6c0c52a0d34df185a3c852317
Sebastian Semmler <sebastian@semmler.pro>
2020-11-07 23:51:33 +0100

    cleanup layout and adding KC_NUBS

--- keyboards/dz60/keymaps/SebSemmi/keymap.c
+++ keyboards/dz60/keymaps/SebSemmi/keymap.c
@@ -1,7 +1,6 @@
 #include QMK_KEYBOARD_H
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-
   /* ISO 60 layout by SebSemmi (ISO macOS keyboard layout shown)
    *
    * This layout starts from a standard ISO 60% layout, and adds two function layer.
@@ -18,21 +17,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * | LCTL  | LOPT  | LCMD  |            Space                  | RCMD  | ROPT  | LAY1 | APFN |
    * `-----------------------------------------------------------------------------------------'
    */
-  [0] = LAYOUT_60_iso(
-    KC_GESC,  KC_1,  KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9,    KC_0,    KC_MINS,  KC_EQL,  KC_BSPC,
-    KC_TAB,  KC_Q,  KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O,    KC_P,    KC_LBRC,  KC_RBRC,
-    MT(MOD_HYPR, KC_CAPS), KC_A,  KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L,    KC_SCLN, KC_QUOT,  KC_BSLS, KC_ENT,
-    KC_LSFT, KC_NUBS, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,
-    KC_LCTL, KC_LALT, KC_LGUI,                   KC_SPC,      KC_RGUI, KC_RALT, MO(1),    KC_APFN
-  ),
+ [0] = LAYOUT_60_iso(
+    KC_GESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC,
+    KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC,
+    MT(MOD_HYPR, KC_CAPS), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_BSLS, KC_ENT,
+    LSFT_T(KC_NUBS), KC_NO, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
+    KC_LCTL, KC_LALT, KC_LGUI, KC_SPC, KC_RGUI, KC_RALT, MO(1), KC_APFN
+ ),
 
-
-  [1] = LAYOUT_60_iso(
-    KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,  KC_F5, KC_F6,  KC_F7, KC_F8,  KC_F9,    KC_F10,  KC_F11,  KC_F12,   KC_TRNS,
-    KC_TRNS, KC_HOME, KC_UP,   KC_END,  KC_NO,  KC_NO, KC_NO,  KC_NO, KC_NO,  KC_NO,    KC_NO,   RGB_MOD, RGB_RMOD,
-    KC_TRNS, KC_LEFT, KC_DOWN, KC_RGHT, KC_NO,  KC_NO, KC_NO,  KC_NO, KC_NO,  RGB_HUI,  RGB_SAI, RGB_VAI,           KC_TRNS,
-    KC_TRNS, KC_NO,   KC_NUBS,   KC_NO,   KC_NO,  KC_NO, KC_NO,  KC_NO, KC_NO,  KC_NO,    BL_ON,   BL_OFF,  KC_NO,    KC_TRNS,
-    KC_TRNS, KC_TRNS, KC_TRNS,                   KC_SPC,                                KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS
-  ),
+ [1] = LAYOUT_60_iso(
+    KC_TRNS, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_TRNS,
+    KC_TRNS, KC_HOME, KC_UP, KC_END, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, RGB_MOD, RGB_RMOD,
+    KC_TRNS, KC_LEFT, KC_DOWN, KC_RGHT, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, RGB_HUI, RGB_SAI, RGB_VAI, KC_TRNS,
+    KC_TRNS, KC_NO, KC_NUBS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, BL_ON, BL_OFF, KC_NO, KC_TRNS,
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_SPC, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+ ),
 
 };
