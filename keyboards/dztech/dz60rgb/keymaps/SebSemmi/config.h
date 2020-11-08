8cf4add4b62600d57df37f7281eddcebdcf90fd4
Sebastian Semmler <sebastian@semmler.pro>
2020-11-08 11:13:01 +0100

    set Grave Escape exceptions

--- keyboards/dztech/dz60rgb/keymaps/SebSemmi/config.h
+++ keyboards/dztech/dz60rgb/keymaps/SebSemmi/config.h
@@ -8,3 +8,7 @@
 /**
 PRODUCT_ID 0x024f is Apple Keyboard with ANSI Layout
 */
+#define GRAVE_ESC_ALT_OVERRIDE
+#define GRAVE_ESC_CTRL_OVERRIDE
+#define GRAVE_ESC_GUI_OVERRIDE
+#define GRAVE_ESC_SHIFT_OVERRIDE
