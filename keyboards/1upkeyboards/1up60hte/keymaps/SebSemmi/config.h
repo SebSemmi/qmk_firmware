0736741c4cb9e12b6630fc7459a466f8622bed09
Sebastian Semmler <sebastian@semmler.pro>
2020-12-06 12:52:47 +0100

    added 1UPHTE support with leader key

---
+++ keyboards/1upkeyboards/1up60hte/keymaps/SebSemmi/config.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#undef VENDOR_ID
+#undef PRODUCT_ID
+
+/*Apple Keyboard with ANSI Layout */
+#define VENDOR_ID 0x05ac
+#define PRODUCT_ID 0x024f
+
+/*Leader Key */
+#define LEADER_TIMEOUT 250
+#define LEADER_PER_KEY_TIMING
