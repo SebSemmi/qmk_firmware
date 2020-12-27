f0c528cc31788df1835d413614b5ef7e33b2f39f
Sebastian Semmler <sebastian@semmler.pro>
2020-12-27 13:43:07 +0100

    define new layer 2 standard

--- keyboards/dz60/keymaps/SebSemmi/config.h
+++ keyboards/dz60/keymaps/SebSemmi/config.h
@@ -7,6 +7,7 @@
 #define VENDOR_ID 0x05ac
 #define PRODUCT_ID 0x0221
 
+/*Features */
+#define TAPPING_TOGGLE 2
 #define GRAVE_ESC_ALT_OVERRIDE
-#define GRAVE_ESC_CTRL_OVERRIDE
 #define GRAVE_ESC_GUI_OVERRIDE
